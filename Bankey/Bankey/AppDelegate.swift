f6b5e3051d0dd55da79ddea6a932ecfe3cf9bfd8
Tyler Madonna <tyler@tylermadonna.com>
2022-07-28 22:11:28 -0400

    Created initial project

---
+++ Bankey/Bankey/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Bankey
+//
+//  Created by Tyler Madonna on 7/28/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = ViewController()
+
+        return true
+    }
+
+}
